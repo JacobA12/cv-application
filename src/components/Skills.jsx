6b8481df06078e3b3c263ee9efff822361efe49a
Jacob Adams big-boi <jacob.adams121202@gmail.com>
2024-12-16 23:40:53 -0800

    Added editing to skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
-import "../styles/skills.css"
+import "../styles/skills.css";
 function Skills() {
-  const [skills, setSkills] = useState([{skill: ""}]);
+  const [skills, setSkills] = useState([{ skill: "" }]);
+  const [isEditing, setIsEditing] = useState(true);
 
   const handleChange = (index, e) => {
     const { name, value } = e.target;
@@ -10,23 +11,43 @@ function Skills() {
     setSkills(newSkills);
   };
 
+  const handleSubmit = () => {
+    setIsEditing(false);
+  };
+
+  const handleEdit = () => {
+    setIsEditing(true);
+  };
+
   const addSkill = () => {
-    setSkills([...skills, { skill: ""}]);
+    setSkills([...skills, { skill: "" }]);
   };
 
   return (
     <div className="skills">
       <h2>Skills</h2>
-      {skills.map((skill,index) => (
-        <input
-        key = {index}
-        name="skill"
-        type="text"
-        placeholder="Skill"
-        value={skill.skill}
-        onChange={(e) => handleChange(index, e)} />
-      ))}
-      <button onClick={addSkill}>Add Skill</button>
+      {isEditing ? (
+        <div>
+          {skills.map((skill, index) => (
+            <input
+              key={index}
+              name="skill"
+              type="text"
+              placeholder="Skill"
+              value={skill.skill}
+              onChange={(e) => handleChange(index, e)}
+            />
+          ))}
+          <button onClick={addSkill}>Add Skill</button>
+        </div>
+      ) : (
+        <div>
+          {skills.map((skill, index) => (
+            <p key={index}>Skill: {skill.skill}</p>
+          ))}
+          <button onClick={handleEdit}>Edit</button>
+        </div>
+      )}
     </div>
   );
 }
