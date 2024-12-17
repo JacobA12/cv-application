76fa059a93d244101d90b4d1115a5a71f199ea74
Jacob Adams big-boi <jacob.adams121202@gmail.com>
2024-12-16 23:27:54 -0800

    added editing to education

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,40 +1,62 @@
 import React, { useState } from "react";
-import "../styles/education.css"
+import "../styles/education.css";
+
 function Education() {
   const [education, setEducation] = useState({
     school: "",
     title: "",
     date: "",
   });
+  const [isEditing, setIsEditing] = useState(true);
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setEducation({ ...education, [name]: value });
+  };
+
+  const handleSubmit = () => {
+    setIsEditing(false);
+  };
+
+  const handleEdit = () => {
+    setIsEditing(true);
   };
   return (
     <div class="education">
       {" "}
       <h2>Educational Experience</h2>{" "}
-      <input
-        type="text"
-        name="school"
-        placeholder="School Name"
-        value={education.school}
-        onChange={handleChange}
-      />{" "}
-      <input
-        type="text"
-        name="title"
-        placeholder="Title of Study"
-        value={education.title}
-        onChange={handleChange}
-      />{" "}
-      <input
-        type="date"
-        name="date"
-        placeholder="Date of Study"
-        value={education.date}
-        onChange={handleChange}
-      />{" "}
+      {isEditing ? (
+        <div>
+          <input
+            type="text"
+            name="school"
+            placeholder="School Name"
+            value={education.school}
+            onChange={handleChange}
+          />{" "}
+          <input
+            type="text"
+            name="title"
+            placeholder="Title of Study"
+            value={education.title}
+            onChange={handleChange}
+          />{" "}
+          <input
+            type="date"
+            name="date"
+            placeholder="Date of Study"
+            value={education.date}
+            onChange={handleChange}
+          />
+        </div>
+      ) : (
+        <div>
+          <p>School: {education.school}</p>
+          <p>Title: {education.title}</p>
+          <p>Date: {education.date}</p>
+          <button onClick={handleEdit}>Edit</button>
+        </div>
+      )}{" "}
     </div>
   );
 }
