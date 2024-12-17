c7dc5f906307f272b02102528895f643b739e848
JacobA12 <jacob.adams121202@gmail.com>
2024-12-16 20:06:05 -0800

    general info

--- src/components/GeneralInfo.jsx
+++ src/components/GeneralInfo.jsx
@@ -1,4 +1,38 @@
-import React, { useState } from 'react';
-function GeneralInfo(){
-  
+import React, { useState } from "react";
+function GeneralInfo() {
+  const [info, setInfo] = useState({ name: "", email: "", phone: "" });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setInfo({ ...info, [name]: value });
+  };
+
+  return (
+    <div>
+      <h2>General Information</h2>
+      <input
+        type="text"
+        name="name"
+        placeholder="Name"
+        value={info.name}
+        onChange={handleChange}
+      ></input>
+      <input
+        type="email"
+        name="email"
+        placeholder="Email"
+        value={info.email}
+        onChange={handleChange}
+      ></input>
+      <input
+        type="tel"
+        name="phone"
+        placeholder="Phone"
+        value={info.tel}
+        onChange={handleChange}
+      ></input>
+    </div>
+  );
 }
+
+export default GeneralInfo;
