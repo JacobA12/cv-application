c487e4b088c49e2669ef7edee311826d421414b1
JacobA12 <jacob.adams121202@gmail.com>
2024-12-16 20:11:33 -0800

    basic inputs on page

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import GeneralInfo from "./components/GeneralInfo";
+// import Education from "./components/Education";
+// import Experience from "./components/Experience";
+
+function App() {
+  return (
+    <div>
+      {" "}
+      <h1>CV Application</h1> <GeneralInfo /> {" "}
+    </div>
+  );
+}
+export default App;
